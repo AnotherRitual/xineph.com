d6c94a27f9eb990912845d3c873f2f72dd20af73
mggk <mggk@protonmail.ch>
2020-03-28 23:10:01 +0100

    lkfdgikotioptzrerobvxcbx

--- 251415211/index.php
+++ 251415211/index.php
@@ -25,9 +25,36 @@
                 color: #fff;
                 font-family: 'IBM Plex Mono', monospace;
             }
+            .GhtyPoGTNJ > *{
+                letter-spacing: 0.5em;
+                margin-top: 0.5em;
+                margin-bottom: 0.5em;
+            }
+            .GhtyPoGTNJ > *:hover {
+                animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+                letter-spacing: 1em;
+            }
+            @keyframes shake {
+                10%, 90% {
+                    transform: translate3d(-1px, 0, 0);
+                }
+                
+                20%, 80% {
+                    transform: translate3d(2px, 0, 0);
+                }
+
+                30%, 50%, 70% {
+                    transform: translate3d(-4px, 0, 0);
+                }
+
+                40%, 60% {
+                    transform: translate3d(4px, 0, 0);
+                }
+            }
         </style>
     </head>
     <body>
+        <audio id="fhfhgdfteriuhg" src="fhfhgdfteriuhg.mp3" loop></audio>
         <div class="GhtyPoGTNJ">
             <div croxhuthwavvnpgibyxf="liwggaidasrovbaaancqt">YFVZTTAROJTXASEAFURD</div>
             <div aqmbbwlqproxxovmxzlo="pnxntrohhcjndqcraqtot">WDWGNYANAWUSTROSFHQS</div>
@@ -40,5 +67,14 @@
             <div wfxsnkbutfsdnnrobjuq="vpukzdpehrocxaqlieek">PPHXZQJLMGBZBYROYPJG</div>
             <div kgqarpyrobhuglqnptxr="narhxlntsrovcgfxkblb">RKECRONPNBNJFONKQDLQ</div>
         </div>
+        <script>
+            document.addEventListener('mousemove', function(){
+                kgdsmgk = document.getElementById("fhfhgdfteriuhg");
+                if (kgdsmgk.duration > 0 && !kgdsmgk.paused) {
+                    } else {
+                        kgdsmgk.play();
+                    }
+            });
+        </script>
     </body>
 </html>
